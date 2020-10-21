9cbaa5ae220161d925d85c61f3394772460a6e00
RaulTomaz <raul.tomaz.rt@gmail.com>
2020-10-20 23:28:04 -0300

    refining runner class with correct stepsDefinitions package name

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
-                 glue = "stepDefinitions",
+                 glue = "stepsDefinitions",
                  plugin = {"pretty", "html:target/report-html"})
 public class Runner {
 }
